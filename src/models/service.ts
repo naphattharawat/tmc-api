beb65cff3bef5a0782bb3abfe4ee3ccc20375d4d
naphattharawat <naphattharawat@gmail.com>
2020-09-23 15:12:16 +0700

    url

--- src/models/service.ts
+++ src/models/service.ts
@@ -6,10 +6,11 @@ export class ServiceModel {
   verifyTMC(firstName, lastName, code = null) {
     const key = process.env.API_TMC_KEY;
     const api_tmc_url = process.env.API_TMC_URL;
-    let url = `${api_tmc_url}/${key}/${firstName}/${lastName}`;
+    let url = `${api_tmc_url}/${key}?p${firstName}!${lastName}`;
     if (code) {
       url += `/${code}`;
     }
+    console.log(url);
     return new Promise((resolve: any, reject: any) => {
       var options = {
         'method': 'GET',
