1aefcb2d0164c10400acfa057ce3d9e6659b7a14
naphattharawat <naphattharawat@gmail.com>
2021-02-15 12:45:42 +0700

    update login

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -15,35 +15,39 @@ const jwt = new Jwt();
 
 const router: Router = Router();
 
-router.post('/', async (req: Request, res: Response, ) => {
+router.post('/', async (req: Request, res: Response,) => {
   let username: string = req.body.username;
   let password: string = req.body.password;
 
   let db = req.db;
   try {
-    let encPassword = crypto.createHash('md5').update(password).digest('hex');
-    let rs: any = await loginModel.login(db, username, encPassword);
+    if (username && password) {
+      let encPassword = crypto.createHash('md5').update(password).digest('hex');
+      let rs: any = await loginModel.login(db, username, encPassword);
 
-    if (rs.length) {
-      let payload = {
-        vender_name: `${rs[0].vender_name}`,
-        id: rs[0].id
-      }
-
-      let token = jwt.signApiKey(payload);
-      if (token) {
-        let dataLogs = {
-          token: token,
-          actived: moment().format('YYYY-MM-DD HH:m:s'),
-          expired: moment().add('d',1).format('YYYY-MM-DD HH:m:s'),
-          use_id: rs[0].id
+      if (rs.length) {
+        let payload = {
+          vender_name: `${rs[0].vender_name}`,
+          id: rs[0].id
         }
-        await loginModel.logs_token(db, dataLogs)
+
+        let token = jwt.signApiKey(payload);
+        if (token) {
+          let dataLogs = {
+            token: token,
+            actived: moment().format('YYYY-MM-DD HH:m:s'),
+            expired: moment().add('d', 1).format('YYYY-MM-DD HH:m:s'),
+            use_id: rs[0].id
+          }
+          await loginModel.logs_token(db, dataLogs)
+        }
+        await loginModel.updateLastActive(db, rs[0].id, { last_actived: moment().format('YYYY-MM-DD HH:m:s') })
+        res.send({ ok: true, token: token, code: HttpStatus.OK });
+      } else {
+        res.send({ ok: false, error: 'Login failed!', code: HttpStatus.UNAUTHORIZED });
       }
-      await loginModel.updateLastActive(db, rs[0].id, {last_actived: moment().format('YYYY-MM-DD HH:m:s')})
-      res.send({ ok: true, token: token, code: HttpStatus.OK });
     } else {
-      res.send({ ok: false, error: 'Login failed!', code: HttpStatus.UNAUTHORIZED });
+      res.send({ ok: false, error: 'EMPTY_PARAMETER', code: HttpStatus.BAD_REQUEST });
     }
   } catch (error) {
     res.send({ ok: false, error: error.message, code: HttpStatus.INTERNAL_SERVER_ERROR });
